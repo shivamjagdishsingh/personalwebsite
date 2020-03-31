87235b80a6cbc771b7ca423c3f0358f0c88a0bd9
shivam singh <33157915+hackedaccount@users.noreply.github.com>
2020-04-01 05:02:57 +0530

    day2, inheritance added to theme, inherited from base.html,still static.

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,10 @@
 from about import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'about'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html")),
     path('webdesigning', views.WebDesigning.as_view(), name='webdesigning'),
     path('softwaredevelopment', views.SoftwareDevelopment.as_view(), name='softwaredevelopment'),
     path('machinelearning', views.MachineLearning.as_view(), name='machinelearning'),
--- thehackedaccount/settings.py
+++ thehackedaccount/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'thehackedaccount.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_ROOT = ''
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_DIRS = (os.path.join('static'),)
